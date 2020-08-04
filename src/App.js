60d7ad68f50e4981acd2e842caecc8318229c24e
i5possible <lianghongbuaa@gmail.com>
2020-08-05 00:07:45 +0800

    show the pdf

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {degrees, PDFDocument, rgb, StandardFonts} from 'pdf-lib';
+import {Document, Page, pdfjs} from 'react-pdf';
+// import sample from '../static/sample.pdf'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const pdfUrl = 'https://i5possible.github.io/assets/cheat-sheet/github-git-cheat-sheet.pdf';
+
+const App = () => {
+
+    const [pdfDoc, setPdfDoc] = useState(null)
+
+    useEffect(() => {
+        const fetchPdf = async () => {
+            const existingPdfBytes = await fetch(pdfUrl).then((res) => res.arrayBuffer());
+            const pdfDoc = await PDFDocument.load(existingPdfBytes)
+// Embed the Helvetica font
+            const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
+
+// Get the first page of the document
+            const pages = pdfDoc.getPages()
+            console.log(pages);
+            const firstPage = pages[0]
+
+// Get the width and height of the first page
+            const {width, height} = firstPage.getSize()
+
+// Draw a string of text diagonally across the first page
+            firstPage.drawText('This text was added with JavaScript!', {
+                x: 5,
+                y: height / 2 + 300,
+                size: 50,
+                font: helveticaFont,
+                color: rgb(0.95, 0.1, 0.1),
+                rotate: degrees(-45),
+            })
+
+            setPdfDoc(pdfDoc)
+        }
+        // fetchPdf();
+    }, [])
+
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+        setPageNumber(1);
+    }
+
+    function changePage(offset) {
+        setPageNumber(prevPageNumber => prevPageNumber + offset);
+    }
+
+    function previousPage() {
+        changePage(-1);
+    }
+
+    function nextPage() {
+        changePage(1);
+    }
+
+    return (
+        <>
+            <Document
+                file={pdfUrl}
+                onLoadSuccess={onDocumentLoadSuccess}
+            >
+                <Page pageNumber={pageNumber} renderTextLayer={false}/>
+            </Document>
+            <div>
+                <p>
+                    Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
+                </p>
+                <button
+                    type="button"
+                    disabled={pageNumber <= 1}
+                    onClick={previousPage}
+                >
+                    Previous
+                </button>
+                <button
+                    type="button"
+                    disabled={pageNumber >= numPages}
+                    onClick={nextPage}
+                >
+                    Next
+                </button>
+            </div>
+        </>
+    );
 }
 
 export default App;
